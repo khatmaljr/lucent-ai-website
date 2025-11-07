b7317a4480a46d6b87a5810efa605b558df14d64
khatmaljr <rohitanand@lucent-ai.net>
2025-11-07 13:16:14 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+typescript 
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import About from "./pages/About";
+import PlaybookPage from "./pages/PlaybookPage";
+import SuccessPage from "./pages/SuccessPage";
+import AssessmentPage from "./pages/AssessmentPage";
+import ThankYouPage from "./pages/ThankYouPage";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <HashRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/playbook" element={<PlaybookPage />} />
+            <Route path="/assessment" element={<AssessmentPage />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="/thank-you" element={<ThankYouPage />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </HashRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
